8edbfb1128869aca22342af06703e0788c742985
이한별 (Hanbyeol Lee) <54656519+hyonestar19@users.noreply.github.com>
2022-08-17 10:48:58 +0900

    MQ-3's working code
    First upload this project

---
+++ alco.ino
@@ -0,0 +1,20 @@
+//int buttonPin = 3;
+int mq3Pin = A5;   // MQ-3 센서핀을 아두이노 보드의 A5 핀으로 설정
+
+void setup(){
+  Serial.begin(9600);          // 아두이노 보드와 PC간의 통신속도 설정
+//  pinMode(buttonPin, INPUT);
+}
+
+
+void loop(){
+  unsigned long val = analogRead(mq3Pin);
+  Serial.println(val);   // MQ-3 센서 출력값을 시리얼 모니터로 출력
+  delay(1000);
+//  if (digitalRead(buttonPin) == HIGH) {
+//    
+//  }
+//  else {
+//    Serial.println("Disread");
+//  }
+}
