e9a836f4d778731cc9bc1a3dfc0c8b5689065bf2
yoBBongE <111402887+yoBBongE@users.noreply.github.com>
2022-08-22 11:16:52 +0900

    Update alco.ino
    led 기능 합병. mode2 부분은 보류

--- alco.ino
+++ alco.ino
@@ -1,16 +1,24 @@
 #include<LiquidCrystal_I2C.h>          //라이브러리 다운로드 필요
 #define aLOW 340
 #define aHIGH 680
-//int buttonPin = 3;
 
+int green = 8;
+int yellow = 9;
+int red = 10;
+int speaker = 3; 
+int mq3Pin = A3; 
 int mode = 12;                        // 토글 스위치에 따른 인풋. 0일 때 1번 모드, 1일때 2번 모드를 적용하도록 하자.
 LiquidCrystal_I2C lcd(0x3F, 16, 2);   // lcd 객체선언 : 혹시 제대로 작동 안 한다면 lcd 주소값 0x3F를 0x27로 고치기
-int mq3Pin = A3;                      // MQ-3 센서핀을 아두이노 보드의 A3 핀으로 설정
 
 void setup(){
   Serial.begin(9600);                 // 아두이노 보드와 PC간의 통신속도 설정
   lcd.begin();
-  pinMode(mode, INPUT_PULLUP);          
+  pinMode(mode, INPUT_PULLUP);    
+  pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(speaker, OUTPUT);
+  pinMode(A3, INPUT); 
 }
 
 
@@ -19,7 +27,34 @@ void loop(){
   Serial.println(val);   // MQ-3 센서 출력값을 시리얼 모니터로 출력
   delay(1000);
   
-  if(digitalRead(mode) == 0){
+  if(digitalRead(mode)==0){
+    if(val <= aLOW){
+      digitalWrite(green, HIGH);
+      digitalWrite(yellow, LOW);
+      digitalWrite(red, LOW);
+      tone(12, 262, 2000);     
+      delay(1000);             
+      noTone(12); 
+    }
+    else if(val <= aHIGH){
+      digitalWrite(8, LOW);  
+      digitalWrite(9, HIGH);
+      digitalWrite(10, LOW) 
+
+      tone(12, 349, 2000);     
+      delay(1000);             
+     noTone(12);    
+    }
+    else{
+      digitalWrite(8, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(10, HIGH);
+
+      tone(12, 494, 2000);     
+      delay(1000);             
+      noTone(12); 
+    }
+    
     lcd.setCursor(0,0);
     lcd.print("MODE 1");
     lcd.setCursor(0,1);
